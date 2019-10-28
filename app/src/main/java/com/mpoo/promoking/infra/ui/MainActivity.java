2385ef4a5c8f0bd4060ad0f5f90d9c97561984be
MrHenri <henrique.oliveira.gomes@gmail.com>
2019-10-28 15:31:45 -0300

    Toolbar

--- app/src/main/java/com/mpoo/promoking/infra/ui/MainActivity.java
+++ app/src/main/java/com/mpoo/promoking/infra/ui/MainActivity.java
@@ -1,10 +1,17 @@
 package com.mpoo.promoking.infra.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.mpoo.promoking.R;
+import com.mpoo.promoking.usuario.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        toolbar.setTitle("PromoKING");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                deslogar();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogar() {
+        try{
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
